6c66b2524740f6fdd3a5d93bce61ef96e0d2d378
chrislennon <chrislennon@users.noreply.github.com>
2017-06-20 23:10:52 +0200

    adds downloads

--- entry_point.sh
+++ entry_point.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Having o work around the fact that you can only have one extension script in Linux on Azure https://stackoverflow.com/a/42965789
+# Having to work around the fact that you can only have one extension script in Linux on Azure https://stackoverflow.com/a/42965789
 
 while [[ $# > 0 ]]
 do
@@ -15,11 +15,21 @@ do
       vm_private_ip="$1"
       shift
       ;;
+    --repository_location|-rp)
+      repository_location="$1"
+      shift
+      ;;
     *)
       echo "ERROR: Unknown argument '$key' to script '$0'" 1>&2
       exit -1
   esac
 done
+
+jenkins_script = "${repository_location}install_jenkins.sh"
+prudentia_script = "${repository_location}install_prudentia.sh"
+
+curl -O $jenkins_script
+curl -O $prudentia_script
 
 ./jenkins.sh -jf $jenkins_fqdn -pi $vm_private_ip
 
