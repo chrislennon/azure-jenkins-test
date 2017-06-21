9ded8af0af48506e1816150b6c179c85320be5c7
chrislennon <chrislennon@users.noreply.github.com>
2017-06-21 13:00:56 +0200

    python2 switch over
    looks like prudentia is primarily written for python 2.7, change steps required to accomodate

--- install_prudentia.sh
+++ install_prudentia.sh
@@ -2,13 +2,23 @@
 
 # OS
 sudo apt-get update --yes
-sudo apt-get -y upgrade
+#sudo apt-get -y upgrade
 
 # Python install
-sudo apt-get install -y python3-dev python3-pip python-dev build-essential 
+sudo apt-get install -y python-dev python-pip python-dev build-essential 
 
 # Prudentia dependancy install
 sudo apt-get install -y libffi-dev libssl-dev
 
+# Upgrade pip to latest
+pip install --upgrade pip
+
 # Install prudentia under the jenkins user
 sudo -H -u jenkins bash -c 'pip3 install prudentia' # This should probably be part of each jenkins job as a virtualenv 
+
+# Place the path for pip binaries into $PATH for user
+
+export LANG=en_US.UTF-8 >> ~/.bash.rc
+export LANGUAGE=en_US:en >> ~/.bash.rc
+export PATH=/var/lib/jenkins/.local/bin:$PATH >> ~/.bash.rc
+source ~/.bash.rc
