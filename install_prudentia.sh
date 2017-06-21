a23ecee089f1b488e3fda80dd8a8341e7cd914c0
chrislennon <chrislennon@users.noreply.github.com>
2017-06-21 13:11:42 +0200

    use full path to jenkins home, also run update under jenkins user

--- install_prudentia.sh
+++ install_prudentia.sh
@@ -11,14 +11,14 @@ sudo apt-get install -y python-dev python-pip python-dev build-essential
 sudo apt-get install -y libffi-dev libssl-dev
 
 # Upgrade pip to latest
-pip install --upgrade pip
+sudo -H -u jenkins bash -c 'pip install --upgrade pip'
 
 # Install prudentia under the jenkins user
 sudo -H -u jenkins bash -c 'pip install prudentia' # This should probably be part of each jenkins job as a virtualenv 
 
 # Place the path for pip binaries into $PATH for user
 
-sudo -H -u jenkins bash -c 'export LANG=en_US.UTF-8 >> ~/.bash.rc'
-sudo -H -u jenkins bash -c 'export LANGUAGE=en_US:en >> ~/.bash.rc'
-sudo -H -u jenkins bash -c 'export PATH=/var/lib/jenkins/.local/bin:$PATH >> ~/.bash.rc'
-sudo -H -u jenkins bash -c 'source ~/.bash.rc'
+sudo -H -u jenkins bash -c 'export LANG=en_US.UTF-8 > /var/lib/jenkins/.bash.rc'
+sudo -H -u jenkins bash -c 'export LANGUAGE=en_US:en >> /var/lib/jenkins/.bash.rc'
+sudo -H -u jenkins bash -c 'export PATH=/var/lib/jenkins/.local/bin:$PATH >> /var/lib/jenkins/.bash.rc'
+sudo -H -u jenkins bash -c 'source /var/lib/jenkins/.bash.rc'
